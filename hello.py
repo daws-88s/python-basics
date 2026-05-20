85a1c0fc4ef2194d7502fc7349f8cd519cb35694
Pavan Kumar Akula <akulapavankumar@outlook.com>
2026-05-20 19:33:37 +0530

    Add python scripts

---
+++ hello.py
@@ -0,0 +1,17 @@
+print("Hello Python!!!")
+
+# fruits = ["apple", "banana", "orange", "mango"]
+# for fruit in fruits:
+#     print(fruit)
+
+# Variables, datatypes
+# Conditions
+# Loops
+# Functions
+
+# Object Oriented Programming (OOP)
+# Multi threading, multi processing
+# working with 3rd party libraries
+
+# Python files should always end with ".py" extension
+# only then python compiler can understand the statements provided in the file
